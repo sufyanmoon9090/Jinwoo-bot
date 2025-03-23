07b0fcfaed908095da15e27bc4e9f9bf309e849f
sufyanmoon9090 <sufyanmoon9090@gmail.com>
2025-03-23 14:30:46 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -16,7 +16,7 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 //--------------------[ SESSION ID ]----------------------//
 
-global.SESSION_ID = process.env.SESSION_ID || '' 
+global.SESSION_ID = process.env.SESSION_ID || 'MALVIN-XD~RkwT3CIZ#3h4Wyr1VwjdI3uXnTTmrHKV-2mpJDyaw_EEjBTPZTkc' 
 //Enter your Jinwoo session id here; must start with MALVIN-XD~
 
 //--------------------[ BOT NAME ]----------------------//
@@ -25,7 +25,7 @@ global.botname = process.env.BOT_NAME || 'ᴊɪɴᴡᴏᴏ-ʙᴏᴛ'
 
 //-----------------[ OWNER NUMBER ]------------------//
 
-global.ownernumber = process.env.OWNER_NUMBER || '263714757857' 
+global.ownernumber = process.env.OWNER_NUMBER || '923276254377' 
 
 //-----------------[ OWNER NAME ]------------------//
 
